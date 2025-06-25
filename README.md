20e9fd1f8150a5f1e778817a83e55e63a143c54b
Neal Lindsay <neal.lindsay@gmail.com>
2025-06-25 10:41:08 -0400

    Add option for limits greater than one
    Make lockify more general so it can support more concurrency than just a
    single task.
    
    Also added some examples to the README.md

--- README.md
+++ README.md
@@ -1,5 +1,47 @@
 # Lockify
-## Use promises to ensure a function doesn't execute concurrently
+## Leverage promises to ensure a function doesn't execute concurrently
 
 Most asynchronous functions are fine to run concurrently with themselves.
-But when they're not - lockify them.
+But when they're not—lockify them.
+
+---
+
+Lockify takes an async function (or any function that returns a promise), and lets you limit concurrency.
+By default the maximum concurrency is 1.
+
+Let's say we're getting dog descriptions from an API:
+
+```javascript
+const headers = { Authorization: 'Bearer abc123xyz890' }
+function fetchDog(name) {
+  const url = `http://example.com/dog/${name}`
+  return fetch(url, { headers }).then(res => res.json())
+}
+
+const allDogs = Promise.all(
+  ['Fido', 'Princess', 'Fluffy'].map(fetchDog)
+)
+```
+
+Unfortunately this API doesn't allow your key to fetch more than one dog at a time.
+Lockify can help with that:
+
+```javascript
+const headers = { Authorization: 'Bearer abc123xyz890' }
+function fetchDog(name) {
+  const url = `http://example.com/dog/${name}`
+  return fetch(url, { headers }).then(res => res.json())
+}
+
+const politeFetchDog = lockify(fetchDog)
+
+const allDogs = Promise.all(
+  ['Fido', 'Princess', 'Fluffy'].map(politeFetchDog)
+)
+```
+
+Or, if you've paid more for the "fetch two dogs at a time" plan with the dog description service:
+
+```javascript
+const politeFetchDog = lockify(fetchDog, 2)
+```
--- test/lib/lockify.test.js
+++ test/lib/lockify.test.js
@@ -1,6 +1,6 @@
 const assert = require('assert').strict
 
-let lockify, subject, runCount, resolvers, rejecters, f
+let lockify, subject, runCount, controllers, f
 
 const wait = ms => new Promise(resolve => setTimeout(resolve, ms))
 
@@ -9,31 +9,97 @@ module.exports = {
     beforeEach: () => {
       lockify = require('../../')
       runCount = 0
-      resolvers = []
-      rejecters = []
-      f = () => {
-        runCount++
-        return new Promise((resolve, reject) => {
-          resolvers.push(resolve)
-          rejecters.push(reject)
+      controllers = []
+      f = () =>
+        new Promise((resolve, reject) => {
+          runCount++
+          controllers.push({ resolve, reject })
         })
-      }
     },
 
     'with the default concurrency of 1': {
       beforeEach: () => { subject = lockify(f) },
 
-      'waits for the first promise to resolve before a second run': async () => {
+      'waits for the first promise to resolve before a second is run': async () => {
         subject()
         subject()
-
+        subject()
         await wait(1)
+
         assert.equal(runCount, 1)
 
-        resolvers.shift()()
+        controllers.shift().resolve()
+        await wait(1)
+        assert.equal(runCount, 2)
+      },
+
+      'allows an unhandled rejection to occur when appropriate': async () => {
+        let unhandledCount = 0
+        process.on('unhandledRejection', () => unhandledCount += 1)
+
+        subject().catch(() => {}) // first one is handled
+        subject() // second one is unhandled
+        await wait(1)
+
+        controllers.shift().reject(Error('this gets handled'))
+        await wait(1)
+        assert.equal(unhandledCount, 0)
+
+        controllers.shift().reject(Error('this is left unhandled'))
+        await wait(1)
+        assert.equal(unhandledCount, 1)
+      },
+    },
+
+    'with concurrency of 2': {
+      beforeEach: () => { subject = lockify(f, 2) },
+
+      'runs the third function call after the first one fulfills': async () => {
+        subject()
+        subject()
+        subject()
+        subject()
 
         await wait(1)
         assert.equal(runCount, 2)
+
+        controllers.shift().resolve()
+
+        await wait(1)
+        assert.equal(runCount, 3)
+      },
+
+      'runs the third function call after the first one rejects': async () => {
+        let gotError = false
+        subject().catch(() => gotError = true)
+        subject()
+        subject()
+        subject()
+
+        await wait(1)
+        assert.equal(runCount, 2)
+
+        controllers.shift().reject(Error('oh no'))
+
+        await wait(1)
+        assert(gotError)
+        assert.equal(runCount, 3)
+      },
+
+      'runs the third function call after the second one fulfills': async () => {
+        subject()
+        subject()
+        subject()
+        subject()
+
+        await wait(1)
+        assert.equal(runCount, 2)
+
+        controllers.shift() // we leave the first one unresolved
+        controllers.shift().resolve() // and resolve the second one instead
+
+        await wait(1)
+        assert.equal(runCount, 3)
       },
 
       'allows an unhandled rejection to occur when appropriate': async () => {
@@ -49,7 +115,7 @@ module.exports = {
 
         assert.equal(unhandledCount, 0)
 
-        rejecters.shift()(Error('nope'))
+        controllers.shift().reject(Error('nope'))
         await wait(1)
 
         assert.equal(unhandledCount, 1)
